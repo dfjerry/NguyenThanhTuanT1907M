f7d7fbf46450825fd622b349ca71d3fae1424e1e
dfjerry <53176182+dfjerry@users.noreply.github.com>
2019-09-25 11:21:46 +0700

    25/09/2019

--- nonameteam/homejs.js
+++ nonameteam/homejs.js
@@ -1,7 +1,7 @@
 $(".menu-item.parent").bind("mouseenter",function (){
     var li = $(this);
     var sub = li.children(".submenu");
-    sub.slideDown();
+    sub.show();
 });
 $(".menu-item.parent").bind("mouseleave",function (){
     var li = $(this);
@@ -18,3 +18,17 @@ $(".vedautrang").bind("click",function (){
 $(document).ready(function(){
     $('#slider').nivoSlider();
 });
+
+/*google maps*/
+
+
+var map;
+function initMap() {
+    var address = {lat: 21.0288, lng: 105.781},
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: address,
+            zoom: 18,
+        });
+    var marker = new google.maps.Marker({position: address, map:map, label: "Đại học FPT"});
+
+}
