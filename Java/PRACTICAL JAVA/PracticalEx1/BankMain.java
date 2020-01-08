54e7472e58187860c434b729eb440a8e6ae6b12d
dfjerry <53176182+dfjerry@users.noreply.github.com>
2020-01-08 09:14:41 +0700

    PRACTICAL-ADFI

---
+++ Java/PRACTICAL JAVA/PracticalEx1/BankMain.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PracticalEx1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author NguyenThanhTuan
+ */
+public class BankMain {
+
+    public static void main(String[] args) {
+        Bank objBank = new Bank();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter balance: ");
+        objBank.setBalance(scanner.nextDouble());
+        System.out.println("Enter rate: ");
+        objBank.setRate(scanner.nextDouble());
+        System.out.println("Interest: ");
+        System.out.println(objBank.calculateInterest());
+    }
+}
---
+++ Java/PRACTICAL JAVA/PracticalEx2/Flight.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PracticalEx2;
+
+/**
+ *
+ * @author NguyenThanhTuan
+ */
+public class Flight {
+
+    private int number = 0;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    private String destination = "";
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public Flight(int number, String destination) {
+        this.number = number;
+        this.destination = destination;
+    }
+
+    public void displayFlight() {
+        System.out.println(+getNumber() + ", " + getDestination());
+    }
+}
