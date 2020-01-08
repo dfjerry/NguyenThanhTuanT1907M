dd2832b601bfd1828edb67da136fd7c145962d62
dfjerry <53176182+dfjerry@users.noreply.github.com>
2020-01-08 09:05:47 +0700

    Practical-ADFI

---
+++ Java/PRACTICAL JAVA/PracticalEx1/Bank.java
@@ -0,0 +1,41 @@
+package PracticalEx1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Bank {
+    
+    private double balance;
+    public double getBalance() {
+        return balance;
+    }
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+    private double rate;
+    public double getRate() {
+        return rate;
+    }
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public Bank(double balance, double rate) {
+        this.balance = balance;
+        this.rate = rate;
+    }
+    public Bank(){
+        
+    }
+    public double calculateInterest(){
+        double interest = this.getBalance() * ( rate /1200);
+        return interest;
+    }
+}
---
+++ Java/PRACTICAL JAVA/PracticalEx2/FlightMain.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PracticalEx2;
+
+/**
+ *
+ * @author NguyenThanhTuan
+ */
+public class FlightMain {
+    public static void main(String[] args) {
+        Flight objFlight = new Flight(857, "Toronto");
+        objFlight.displayFlight();
+    }
+}
