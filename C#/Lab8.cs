aab2bd73044732e8c7a215aa488b543c63140249
dfjerry <53176182+dfjerry@users.noreply.github.com>
2020-05-07 09:50:34 +0700

    lab8 + ass6

---
+++ C#/Lab8.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+namespace Demo.Session8
+{
+    public class Lab8
+    {
+        public static void Main(string[] args)
+        {
+            Thread t1 = new Thread(ThreadRun);
+            t1.Start(15);//truyen vao 15p
+        }
+
+        public static void ThreadRun(object o)
+        {
+            int n = (int) o;
+            for (int i = n-1; i >= 0; i--)
+            {
+                for (int j = 59; j >= 0; j--)
+                {
+                    Console.WriteLine(i.ToString("D2")+ ":"+j.ToString("D2"));//number format 01 02 in c#
+                    Thread.Sleep(10);
+                }
+            }
+
+            Console.WriteLine("Boom!!!");
+        }
+
+        
+    }
+}
