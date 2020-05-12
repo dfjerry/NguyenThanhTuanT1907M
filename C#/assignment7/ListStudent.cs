6ce635e723a0b7ba0fd2eb0ebf479ec1536ff997
dfjerry <53176182+dfjerry@users.noreply.github.com>
2020-05-12 10:50:20 +0700

    ass7

---
+++ C#/assignment7/ListStudent.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Security.Cryptography;
+namespace Demo.assignment7
+{
+    public class ListStudent
+    {
+        private List<Student> listStudent;
+
+        public ListStudent()
+        {
+            this.listStudent=new List<Student>();
+        }
+
+        public void ShowStudent()
+        {
+            for (int i = 0; i < listStudent.Count; i++)
+            {
+                Console.WriteLine("id :"+listStudent[i].Id+" name: "+listStudent[i].Name+" age: "+listStudent[i].Age+" Address: "+listStudent[i].Address+" gpa: "+listStudent[i].Gpa);
+            }
+        }
+        
+        public void SortGpa()
+        {
+            listStudent.Sort((x, y) => y.Gpa.CompareTo(x.Gpa));
+        }
+        
+        public void SortName()
+        {
+            listStudent.Sort((x, y) => x.Name.CompareTo(y.Name));
+        }
+
+        public Student Search(int id)
+        {
+            for (int i = 0; i < listStudent.Count; i++)
+            {
+                if (id == listStudent[i].Id)
+                {
+                    return listStudent[i];
+                }
+            }
+
+            return null;
+        }
+        public bool DeleteStudent(int id)
+        {
+            Student st = Search(id);
+            if (st != null)
+            {
+                listStudent.Remove(st);
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool EditStudent(int id)
+        {
+            Student st = Search(id);
+            if (st != null)
+            {
+                Console.WriteLine("1: name\n2: age\n3: Address\n4:gpa\n5:complete");
+                Console.WriteLine("Chon phan ma ban muon sua: ");
+                int x;
+                do
+                {
+                    x = Convert.ToInt32(Console.ReadLine());
+                    if (x == 1)
+                    {
+                        Console.WriteLine("Nhap ten can sua");
+                        string y = Convert.ToString(Console.ReadLine());
+                        st.Name = y;
+                    }else if (x == 2)
+                    {
+                        Console.WriteLine("nhap tuoi can sua : ");
+                        int y = Convert.ToInt32(Console.ReadLine());
+                        st.Age = y;
+                    }else if (x == 3)
+                    {
+                        Console.WriteLine("Nhap dia chi moi: ");
+                        string y = Convert.ToString(Console.ReadLine());
+                        st.Address = y;
+                    }else if (x == 4)
+                    {
+                        Console.WriteLine("Nhap diem trung binh moi: ");
+                        float y = Convert.ToInt32(Console.ReadLine());
+                        st.Gpa = y;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Khong co action: ");
+                    }
+
+                } while (x == 5);
+
+                return true;
+
+
+
+            }
+            Console.WriteLine("Khong co id trong danh sach de sua");
+            return false;
+        }
+
+        public bool AddStudent(Student student)
+        {
+            if (Search(student.Id) != null)
+            {
+                Console.WriteLine("student exist in the list, you can not add.");
+                return false;
+            }
+            listStudent.Add(student);
+            Console.WriteLine("You just add to the  list student "+student.Name);
+            return true;
+        }
+        
+    }
+    
+}
---
+++ C#/assignment7/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.CodeDom;
+namespace Demo.assignment7
+{
+    public class Program
+    {
+         private static ListStudent ls = new ListStudent();
+
+        public static void Main(string[] args)
+        {
+            Boolean quit = false;
+            ChooseAction();
+            while(!quit)
+            {
+                Console.WriteLine("Enter action: ");
+                int action = Convert.ToInt32(Console.ReadLine());
+                switch (action)
+                {
+                 case 1: AddSt();
+                     break;
+                 case 2: UpdateSt();
+                     break;
+                 case 3: DeleteSt();
+                     break;
+                 case 4: ls.SortGpa();
+                     break;
+                 case 5:
+                     ls.SortName();
+                     break;
+                 case 6: ls.ShowStudent();
+                     break;
+                 case 7: ChooseAction();
+                     break;
+                 case 8: Console.WriteLine("\nShutting down....");
+                     quit = true;
+                     break;
+                }
+
+            }
+        }
+
+        public static void ChooseAction()
+        {
+            Console.WriteLine("Starting....");
+            Console.WriteLine("Enter 1 to add student");
+            Console.WriteLine("Enter 2 to edit student");
+            Console.WriteLine("Enter 3 to delete student");
+            Console.WriteLine("Enter 4 to sort student by gpa");
+            Console.WriteLine("Enter 5 to sort student by name");
+            Console.WriteLine("Enter 6 to show student");
+            Console.WriteLine("Enter 7 to show action.");
+            Console.WriteLine("Enter 8 to exit");
+        }
+
+        public static void AddSt()
+        {
+            Console.WriteLine("id: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Name: ");
+            string name = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Address: ");
+            string address = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Gpa: ");
+            int gpa = Convert.ToInt32(Console.ReadLine());
+            Student st1 = new Student(id, name, age, address, gpa);
+            ls.AddStudent(st1);
+        }
+
+        public static void UpdateSt()
+        {
+            Console.WriteLine("Enter an Id of student that  you want to edit:");
+            int id = Convert.ToInt32(Console.ReadLine());
+            ls.EditStudent(id);
+        }
+
+        public static void DeleteSt()
+        {
+            Console.WriteLine("Enter id of student that you want to delete: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+            ls.DeleteStudent(id);
+        }
+    }
+}
