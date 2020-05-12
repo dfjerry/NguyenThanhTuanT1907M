4d48683b346a8bf53c6c6e5e382fe86a0a636bf1
dfjerry <53176182+dfjerry@users.noreply.github.com>
2020-05-12 10:29:11 +0700

    practical

---
+++ C#/Practical/Polymorphism/Animal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Demo.Polymorphism
+{
+    public class Animal
+    {
+        protected string name;
+        protected int weight;
+
+        public Animal(string name, int weight)
+        {
+            this.name = name;
+            this.weight = weight;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Weight
+        {
+            get => weight;
+            set => weight = value;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("name: "+Name);
+            Console.WriteLine("Weight: "+weight);
+        }
+
+        public void SetMe(string name, int weight)
+        {
+            this.name = name;
+            this.weight = weight;
+        }
+    }
+}
---
+++ C#/Practical/Polymorphism/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo.Polymorphism
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Lion lion = new Lion("Lion1",200);
+            Tiger tiger = new Tiger("Tiger1",100);
+        }
+    }
+}
